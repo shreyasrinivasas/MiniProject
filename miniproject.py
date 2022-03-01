494bb3cd061178e676f54f8f331a6d862bb4056f
shreyasrinivasas <100700011+shreyasrinivasas@users.noreply.github.com>
2022-03-02 00:28:33 +0530

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,26 @@
+import random
+
+p1=input("Enter (Rock/Paper/Scissors):")
+L1=["Rock","Paper","Scissors"]
+p2=random.choice(L1)
+print("Player 1: ",p1)
+print("Player 2: ",p2)
+
+if(p1 == p2):
+    print("It's a tie!!")
+else:
+    if(p1=="Rock"):
+        if(p2 == "Paper"):
+            print("Player 2 Wins!!")
+        elif(p2== " Scissors"):
+            print("Player 1 Wins!!")
+    elif(p1=="Paper"):
+        if(p2 == "Rock"):
+            print("Player 1 Wins!!")
+        elif(p2== " Scissors"):
+            print("Player 2 Wins!!")
+    elif(p1=="Scissors"):
+        if(p2 == "Rock"):
+            print("Player 2 Wins!!")
+        elif(p2== " Paper"):
+            print("Player 1 Wins!!")
